f7e0e8790102550327bc7951c53d98f7cc3dd55f
Eb <160401318+e43b@users.noreply.github.com>
2024-06-22 08:07:21 -0300

    Update postimg.py

--- codept/code/postimg.py
+++ codept/code/postimg.py
@@ -12,7 +12,18 @@ headers = {
 }
 
 # URL da página que contém a imagem
-url = 'https://br.ifunny.co/picture/6NZWLBIXB'
+url = input("Digite o link do post com imagem: ")
+
+# Variável para definir se deve cortar a logomarca
+cortar_logomarca = True  # Defina como False se não quiser cortar a logomarca
+
+# Nome do diretório onde as imagens serão salvas
+diretorio = 'images'
+
+# Verifica se o diretório existe e cria se não existir
+if not os.path.exists(diretorio):
+    os.makedirs(diretorio)
+    print(f'Diretório "{diretorio}" criado com sucesso.')
 
 # Faça a requisição para obter o HTML da página
 response = requests.get(url, headers=headers)
@@ -35,19 +46,23 @@ if image_tag and 'src' in image_tag.attrs:
     ext = image_url.split('.')[-1].split('?')[0]  # Extrai a extensão da URL da imagem
 
     # Nome do arquivo local com a extensão original
-    filename = os.path.join('images', f"img_{url.split('/')[-1]}.{ext}")
+    filename = os.path.join(diretorio, f"img_{url.split('/')[-1]}.{ext}")
 
     with open(filename, 'wb') as file:
         file.write(image_data)
     print(f'Imagem salva com sucesso como {filename}')
 
-    # Abrir a imagem com PIL para cortar a borda de baixo
-    image = Image.open(BytesIO(image_data))
-    width, height = image.size
-    cropped_image = image.crop((0, 0, width, height - 20))
+    if cortar_logomarca:
+        # Abrir a imagem com PIL para cortar a borda de baixo
+        image = Image.open(BytesIO(image_data))
+        width, height = image.size
+        cropped_image = image.crop((0, 0, width, height - 20))
 
-    # Salvar a imagem cortada de volta no mesmo arquivo
-    cropped_image.save(filename)
-    print(f'Imagem cortada com sucesso e sobrescrita em {filename}')
+        # Salvar a imagem cortada de volta no mesmo arquivo
+        cropped_image.save(filename)
+        print(f'Imagem cortada com sucesso e sobrescrita em {filename}')
+    else:
+        print('Imagem não foi cortada.')
+
 else:
     print('Não foi possível encontrar a imagem.')
